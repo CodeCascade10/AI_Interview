7933a8bd5a9cc1a4b644baa52861dc305a599292
Kausik Naskar <kausiknaskar10@gmail.com>
2026-04-16 20:30:49 +0530

    Fix cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -42,9 +42,12 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-origin: "http://localhost:5173",
-credentials: true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://ai-interview-jade-phi.vercel.app"
+  ],
+  credentials: true
+}));
 
 /**
 
